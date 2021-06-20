78bc6509b6bc8ad26692711297416d553cb7061f
Prince <Prince@LAPTOP-KFIKPE1S>
2021-06-20 15:41:09 +0530

    Spring JDBC with Annotation

---
+++ 05.SpringJDBCAnotation/src/main/java/org/spring/jdbc/configuration/JDBCConfiguration.java
@@ -0,0 +1,45 @@
+package org.spring.jdbc.configuration;
+
+import javax.sql.DataSource;
+
+import org.spring.jdbc.dao.StudentDao;
+import org.spring.jdbc.dao.StudentDaoImp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class JDBCConfiguration {
+
+	@Bean("ds")
+	public DataSource getDataSource() {
+		
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/springjdbc");
+		ds.setUsername("root");
+		ds.setPassword("Prince@123");
+	
+		return ds;
+	}
+	
+	@Bean("jdbcTemplate")
+	public JdbcTemplate getTemplate() {
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(getDataSource());
+		
+		return jdbcTemplate;
+	}
+
+	@Bean("studentDao")
+	public StudentDao getDtudentDao() {
+		
+		StudentDaoImp studentDaoImp = new StudentDaoImp();
+		studentDaoImp.setJdbcTemplate(getTemplate());
+
+		return studentDaoImp;
+	}
+	
+}
